585a438b8e07138fd757fd2b4ba06bffbb88ad27
Aliaa Osman <aliaaosman1995@yahoo.com>
2016-04-13 18:14:43 +0200

    2nd

---
+++ AVL Trees/src/eg/edu/alexu/csd/filestructure/avl/Dictionary.java
@@ -0,0 +1,68 @@
+package eg.edu.alexu.csd.filestructure.avl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Dictionary implements IDictionary {
+
+	private AVLTree<String> myDictionary = new AVLTree<String>();
+	@Override
+	public void load(File file) {
+		// TODO Auto-generated method stub
+		try (BufferedReader br = new BufferedReader(new FileReader(file)))
+		{
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				myDictionary.insert(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+	}
+	
+
+	@Override
+	public boolean insert(String word) {
+		if(myDictionary.search(word))
+		{
+			return false;
+		}
+		myDictionary.insert(word);
+		return true;
+	}
+
+	@Override
+	public boolean exists(String word) {
+		// TODO Auto-generated method stub
+		if(myDictionary.search(word))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delete(String word) {
+		// TODO Auto-generated method stub
+		return myDictionary.delete(word);
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return myDictionary.countNodes(myDictionary.getTree());
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return myDictionary.height();
+	}
+
+}
