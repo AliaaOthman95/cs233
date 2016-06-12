e2dbf80f1fd7cd2431309690dff545399992836a
Aliaa Osman <aliaaosman1995@yahoo.com>
2016-06-13 00:14:39 +0200

    s

--- BinaryHeaps/src/eg/edu/alexu/csd/filestructure/hash/Linear.java
+++ BinaryHeaps/src/eg/edu/alexu/csd/filestructure/hash/Linear.java
@@ -24,23 +24,23 @@ public class Linear<K, V> implements IHash<K, V>, IHashLinearProbing {
 	}
 
 	private void rehash(int cap) {
+		Pair<K, V>[] hashTable2 = hashTable;
+		capacity = cap;
+		// System.arraycopy(hashTable, 0, hashTable2, 0, hashTable.length);
+
+		hashTable = new Pair[capacity];
+		for (int i = 0; i < hashTable2.length; i++) {
+			put(hashTable2[i].getKey(), hashTable2[i].getValue());
+		}
+		
+//		capacity = cap ;
 //		Pair<K, V>[] hashTable2 = new Pair[capacity];
-//		capacity = cap;
-//		// System.arraycopy(hashTable, 0, hashTable2, 0, hashTable.length);
-//
-//		hashTable = new Pair[capacity];
-//		for (int i = 0; i < hashTable2.length; i++) {
-//			put(hashTable2[i].getKey(), hashTable2[i].getValue());
-//		}
-		
-		capacity = cap ;
-		Pair<K, V>[] hashTable2 = new Pair[capacity];
-		
-		for (int i = 0; i < hashTable.length; i++) {
-			put(hashTable[i].getKey(), hashTable[i].getValue());
-	}
-		
-		hashTable = hashTable2 ;
+//		
+//		for (int i = 0; i < hashTable.length; i++) {
+//			put(hashTable[i].getKey(), hashTable[i].getValue());
+//	}
+//		
+//		hashTable = hashTable2 ;
 	}
 
 	@Override
